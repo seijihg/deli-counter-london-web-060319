1f529a7d9c206e087f8d1aefbe2ecdccb06fe6f8
Le H Ngo <seiji@o2.pl>
2019-05-20 22:20:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,13 +11,18 @@ def line(array)
   else
     puts "The line is currently empty."
   end
-
 end
-
 
 def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
+end
 
-
+def now_serving(array)
+  if array.length > 0
+    array.shift
+    puts array.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
 end
