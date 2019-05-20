1521e2a7221a3e5ed870d016d6f62d65011c8cdf
Le H Ngo <seiji@o2.pl>
2019-05-20 22:04:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def line(array)
+  array_n = []
+
+  if array.length > 0
+    array.each_with_index do |name, i|
+      array_n.push("#{i + 1}. #{name}")
+    end
+
+    puts "The line is currently: " + array_n.join(" ")
+
+  else
+    puts "The line is currently empty."
+  end
+
+end
+
+
+def take_a_number(array, name)
+  puts array name
+end
