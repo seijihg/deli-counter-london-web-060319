6ab503fb53479211a7a87943ed648f08a004e944
Le H Ngo <seiji@o2.pl>
2019-05-20 22:10:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,5 @@ end
 
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{array.length} in line."
+  puts "Welcome, #{name}. You are number #{array.length + 1} in line."
 end
